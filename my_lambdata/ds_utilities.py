36cf65c3ed64463b3a6a2da964efe3eae6869bfa
Toby Chen <toby-chen@lambdastudents.com>
2020-07-09 18:48:16 -0400

    Fixed a few things

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,5 +1,3 @@
-import numpy as np
-# import pandas as pd
 def train_val_test_split(df):
   """
   Function will split the data set into train=80, val=10, test=10.
@@ -7,12 +5,12 @@ def train_val_test_split(df):
   """
   train, val, test = np.split(df.sample(frac=1), [int(.8*len(df)), int(.9*len(df))])
   return train, val, test
-  def remove_outliers(df):
+def remove_outliers(df):
   """
   Function will remove 1.5 IQR outliers from data set
   """
-  Q1 = df.quantile(0.25)
-  Q3 = df.quantile(0.75)
-  IQR = Q3 - Q1
-  df = df[~((df < (Q1 - 1.5 * IQR))|(df > (Q3 + 1.5 * IQR))).any(axis=1)]
+  q1 = df.quantile(0.25)
+  q3 = df.quantile(0.75)
+  iqr = q3 - Q1
+  df = df[~((df < (q1 - 1.5 * iqr))|(df > (q3 + 1.5 * iqr))).any(axis=1)]
   return df

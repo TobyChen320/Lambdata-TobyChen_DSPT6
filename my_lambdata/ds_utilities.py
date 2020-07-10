264aa23705212b6435c1a10785d873de94473933
Toby Chen <toby-chen@lambdastudents.com>
2020-07-09 23:04:19 -0400

    Updated

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,8 +1,11 @@
 import numpy as np
+
+
 def train_val_test_split(df):
   """
   Function will split the data set into train=80, val=10, test=10.
-  Note that this is a random split and will not take certain parameters such as date into consideration.
+  Note that this is a random split.
+  It will not take certain parameters such as date into consideration.
   """
   train, val, test = np.split(df.sample(frac=1), [int(.8*len(df)), int(.9*len(df))])
   return train, val, test

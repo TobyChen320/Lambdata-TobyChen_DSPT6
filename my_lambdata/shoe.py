dfbb31f28e86e30e842687369d4141fe8222ac3c
Toby Chen <toby-chen@lambdastudents.com>
2020-07-10 22:24:33 -0400

    Updated

---
+++ my_lambdata/shoe.py
@@ -0,0 +1,8 @@
+class Shoe():
+    def __init__(self, color, size, style=None):
+        self.color = color
+        self.size = size
+        self.style = style
+
+if __name__ == "__main__":
+    shoe1 = Shoe(color="Black", size="11")
